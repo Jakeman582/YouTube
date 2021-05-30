d04cec3f70fb12079efb2248f1ac851a5578ff03
Jacob Hiance <jakehiance@gmail.com>
2021-05-30 01:38:49 -0400

    Added initial template code the the MapReduce driver class

--- src/main/java/YouTubeTFIDF/Driver.java
+++ src/main/java/YouTubeTFIDF/Driver.java
@@ -1,5 +1,20 @@
 package YouTubeTFIDF;
 
-public class Driver {
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class Driver extends Configured implements Tool {
+
+	@Override
+	public int run(String[] args) throws Exception {
+		
+		return 0;
+		
+	}
+	
+	public static void main(String[] args) throws Exception {
+		System.exit(ToolRunner.run(new Driver(), args));
+	}
 
 }
