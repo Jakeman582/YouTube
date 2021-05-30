82daf0357ed161865c4c3b0c81bf0c2bc1eef4d4
Jacob Hiance <jakehiance@gmail.com>
2021-05-30 15:02:13 -0400

    Fixed document counting logic in DocumentCountReducer to use a HashSet

--- src/main/java/YouTubeTFIDF/DocumentCountReducer.java
+++ src/main/java/YouTubeTFIDF/DocumentCountReducer.java
@@ -1,6 +1,8 @@
 package YouTubeTFIDF;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
@@ -9,21 +11,21 @@ import org.apache.hadoop.mapreduce.Reducer;
 public class DocumentCountReducer extends Reducer<NullWritable, Text, NullWritable, NullWritable> {
 	
 	// Keep track of how many documents/YouTubers have been seen
-	int documentCount;
+	Set<Text> documents;
 	
 	// Count the number of documents/YouTubers seen
 	@Override
 	public void reduce(NullWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-		documentCount = 0;
+		documents = new HashSet<Text>();
 		for(Text value : values) {
-			documentCount++;
+			documents.add(value);
 		}
 	}
 	
 	// Write the number of documents seen to the distributed cache
 	@Override
 	public void cleanup(Context context) throws IOException, InterruptedException {
-		context.getConfiguration().set("DOCUMENT_COUNT", "" + documentCount);
+		context.getConfiguration().set("DOCUMENT_COUNT", "" + documents.size());
 	}
 	
 }
